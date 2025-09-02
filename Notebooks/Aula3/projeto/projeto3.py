ebe24b29b5b0fed04d22e926ffb24d62038d3e68
Tiago Tavares <tiagoft@gmail.com>
2025-09-02 13:21:54 -0300

    Adicionei docs no projeto 3

--- Notebooks/Aula3/projeto/projeto3.py
+++ Notebooks/Aula3/projeto/projeto3.py
@@ -1,6 +1,15 @@
 import numpy as np 
 
 def matriz_de_rotacao(angulo: float, centro: np.ndarray) -> np.ndarray:
+    """Calcula uma matriz de rotacao
+
+    Args:
+        angulo (float): Angulo de rotacao, em graus
+        centro (np.ndarray): Centro da imagem, em um array [i,j] em pixels
+
+    Returns:
+        np.ndarray: Matriz em coordenadas homogeneas que realiza a rotacao
+    """
     matriz = np.eye(3)
     return matriz
 
@@ -13,5 +22,18 @@ def criar_indices(min_i, max_i, min_j, max_j):
     return idx
 
 def rotacionar_imagem(imagem: np.ndarray, angulo: float) -> np.ndarray:
+    """Rotaciona a imagem em um determinado ângulo ao redor de seu centro.
+
+    Args:
+        imagem (np.ndarray): Imagem a ser rotacionada.
+        angulo (float): Ângulo de rotação em graus.
+
+    Returns:
+        np.ndarray: Imagem rotacionada.
+    """
+
+    Returns:
+        np.ndarray: _description_
+    """
     imagem_nova = np.zeros_like(imagem)
     return imagem_nova
